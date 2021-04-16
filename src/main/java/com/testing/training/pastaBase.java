31f2c988cb1a647f16d648a1b6f7b47084c2945d
astorlucas <lucasastor@gmail.com>
2021-04-16 00:18:13 -0300

    init

---
+++ src/main/java/com/testing/training/pastaBase.java
@@ -0,0 +1,13 @@
+package com.testing.training;
+
+import org.openqa.selenium.remote.DesiredCapabilities;
+
+public class pastaBase {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		DesiredCapabilities cap = new DesiredCapabilities();
+
+	}
+
+}
