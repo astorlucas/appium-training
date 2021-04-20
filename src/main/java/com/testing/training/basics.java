c67e6901ca4d6d759980dd3c59e9284aa368dcda
astorlucas <lucasastor@gmail.com>
2021-04-19 23:51:44 -0300

    first click

--- src/main/java/com/testing/training/basics.java
+++ src/main/java/com/testing/training/basics.java
@@ -1,6 +1,7 @@
 package com.testing.training;
 
 import java.net.MalformedURLException;
+import java.util.concurrent.TimeUnit;
 
 import io.appium.java_client.android.AndroidDriver;
 import io.appium.java_client.android.AndroidElement;
@@ -10,6 +11,8 @@ public class basics extends pastaBase {
 	public static void main(String[] args) throws MalformedURLException {
 		
 		AndroidDriver<AndroidElement> driver =Capabilities();
+		driver.manage().timeouts().implicitlyWait(10, TimeUnit.SECONDS);
+		driver.findElementByXPath("//android.widget.TextView[@text='Preference']").click();
 
 	}
 
