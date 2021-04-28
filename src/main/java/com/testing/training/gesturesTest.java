dabefda2163c3312545597a89983cc27700211e7
astorlucas <lucasastor@gmail.com>
2021-04-27 23:01:17 -0300

    gesture longpress

--- src/main/java/com/testing/training/gesturesTest.java
+++ src/main/java/com/testing/training/gesturesTest.java
@@ -7,7 +7,9 @@ import org.openqa.selenium.WebElement;
 
 import io.appium.java_client.TouchAction;
 import static io.appium.java_client.touch.TapOptions.tapOptions;
+import static io.appium.java_client.touch.LongPressOptions.longPressOptions;
 import static io.appium.java_client.touch.offset.ElementOption.element;
+import static java.time.Duration.ofSeconds;
 
 
 import io.appium.java_client.android.AndroidDriver;
@@ -20,10 +22,14 @@ public class gesturesTest extends pastaBase {
 		driver.manage().timeouts().implicitlyWait(10, TimeUnit.SECONDS);
 		driver.findElementByXPath("//android.widget.TextView[@text='Views']").click();
 		//Tap
-		//TODO why the hell make WebelEments: to perform specific Appium operations
+		//TODO why the hell make WebelEments: to perform specific Appium mobile operations
 		TouchAction gestures = new TouchAction(driver);
 		WebElement element = driver.findElementByXPath("//android.widget.TextView[@text='Expandable Lists']");
 		gestures.tap(tapOptions().withElement(element(element))).perform();
+		driver.findElementByXPath("//android.widget.TextView[@text='1. Custom Adapter']").click();
+		WebElement element2 = driver.findElementByXPath("//android.widget.TextView[@text='Peoples Names']");
+		//Long press action
+		gestures.longPress(longPressOptions().withElement(element(element2)).withDuration(ofSeconds(2))).release().perform();
 	}
 
 }
