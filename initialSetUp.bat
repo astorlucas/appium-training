6a23c6c6953e85a82f688aaae339297935874d36
astorlucas <lucasastor@gmail.com>
2021-05-11 00:04:38 -0300

    open batch like admin and open init setup

---
+++ initialSetUp.bat
@@ -0,0 +1,23 @@
+@echo off
+if _%1_==_payload_  goto :payload
+
+:getadmin
+    echo %~nx0: elevating self
+    set vbs=%temp%\getadmin.vbs
+    echo Set UAC = CreateObject^("Shell.Application"^)                >> "%vbs%"
+    echo UAC.ShellExecute "%~s0", "payload %~sdp0 %*", "", "runas", 1 >> "%vbs%"
+    "%temp%\getadmin.vbs"
+    del "%temp%\getadmin.vbs"
+goto :eof
+
+:payload
+
+:: This is for the appium and android set up needed
+
+
+
+start appium
+
+
+cd C:\Users\lucas\AppData\Local\Android\Sdk\emulator && emulator -avd emulador1
+
